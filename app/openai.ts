c682bc3b542eb942a28b076e94c723b3f18bc94e
caf112 <yuu10san.14@gmail.com>
2024-06-19 06:02:20 +0900

    openai.tsのコードを省略

--- app/openai.ts
+++ app/openai.ts
@@ -4,19 +4,6 @@ const apiKey = process.env.AZURE_OPENAI_API_KEY;
 const endpoint = process.env.AZURE_OPENAI_ENDPOINT;
 const apiVersion = process.env.AZURE_OPENAI_API_VERSION_ID_1;
 
-if(!apiKey){
-    console.error("APIキーが設定されていません。");
-    process.exit(1);
-}
-if (!endpoint) {
-    console.error("エンドポイントが設定されていません。");
-    process.exit(1);
-}
-if (!apiVersion) {
-    console.error("APIバージョンが設定されていません。");
-    process.exit(1);
-}
-
 export const openai = new AzureOpenAI({
     apiKey:apiKey,
     endpoint:endpoint,
